4fe88491e70705c52c33886b891627e6db96c0ce
Adva Zair <adva@lightricks.com>
2018-11-06 12:17:16 -0500

    debug.

--- prerun.sh
+++ prerun.sh
@@ -2,3 +2,5 @@
 
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/research
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/research/slim
+
+python3 research/deeplab/train.py --train_logdir output/mobile_net --model_variant=mobilenet_v2 --tf_initial_checkpoint=checkpoints/mobilenet_v2_1.0_224/mobilenet_v2_1.0_224.ckpt --train_split=train_aug --dataset_dir=/data/aug_pascal_tfrecord/tfrecord
