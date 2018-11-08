449f1f39b1fb358999c98d73c80c0a4e06ce6a07
Adva Zair <adva@lightricks.com>
2018-11-08 13:05:42 -0500

    debug.

--- prerun.sh
+++ prerun.sh
@@ -1,22 +1,15 @@
 #!/bin/bash -l
 
 printf "PYTHONPATH=$PYTHONPATH\n"
+printf "Adding deeplab and slim to PYTHONPATH\n"
 
-echo 'export PYTHONPATH="/opt/ds3/bin/python"' >> ~/.bashrc
+echo 'export PYTHONPATH=${PYTHONPATH}:$(pwd)/research' >> ~/.bashrc
+echo 'export PYTHONPATH=${PYTHONPATH}:$(pwd)/research/slim' >> ~/.bashrc
+
 source ~/.bashrc
 
 printf "New PYTHONPATH=$PYTHONPATH\n"
-echo "pre run ok"
-
-
-
-
-#printf "PYTHONPATH=$PYTHONPATH\n"
-#printf "Adding deeplab and slim to PYTHONPATH\n"
-
-#export PYTHONPATH=${PYTHONPATH}:$(pwd)/research
-#export PYTHONPATH=${PYTHONPATH}:$(pwd)/research/slim
-
-#printf "New PYTHONPATH=$PYTHONPATH\n"
 
 #source /opt/ds3/bin/activate
+
+
