945d20a3ba3134cd79d333fa821d312871def21d
Adva Zair <adva@lightricks.com>
2020-04-05 22:06:03 +0300

    fix.

--- research/deeplab/utils/save_annotation.py
+++ research/deeplab/utils/save_annotation.py
@@ -126,10 +126,9 @@ def vis_segmentation(image,
   # plt.title('segmentation overlay')
 
   if label_names is not None:
-    label_names = label_names[1:]
     full_color_map = np.arange(len(label_names)).reshape(len(label_names), 1)
     full_color_map = get_dataset_colormap.label_to_color_image(full_color_map, colormap_type)
-    unique_labels = list(set(np.unique(seg_map)) - {0})
+    unique_labels = sorted(list(set(np.unique(seg_map)) - {0}))
     ax = plt.subplot(grid_spec[3])
     plt.imshow(
       full_color_map[unique_labels].astype(np.uint8), interpolation='nearest')
