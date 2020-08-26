b44eefcddb051c1cdb4bb852638ca3e6b3c1cef4
Adva Zair <adva@lightricks.com>
2020-08-26 15:25:33 +0300

    print fix.

--- research/deeplab/datasets/build_image_mask_data.py
+++ research/deeplab/datasets/build_image_mask_data.py
@@ -102,7 +102,7 @@ def _convert_dataset(dataset_split, dataset_dir_list, dataset_label_dir_list, nu
             seg_names.append(seg)
             new_img_names.append(image_file)
         else:
-            print("Couldn't find pair for %s in %s:" % (image_file, dataset_label_dir))
+            print("Couldn't find pair for %s in %s" % (image_file, dataset_label_dir))
       img_names = new_img_names
       all_data_files += list(zip(img_names, seg_names))
     else:
