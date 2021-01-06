f61368b4fe87d9cd3d8468a387e6faea4e9608d8
Adva Zair <adva@lightricks.com>
2021-01-06 08:26:38 +0200

    update prerun and requirements.

--- prerun.sh
+++ prerun.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-apt install -y protobuf-compiler
+apt-get update
+apt-get install -y protobuf-compiler ffmpeg libsm6 libxext6
 cd /cnvrg/research
 protoc object_detection/protos/*.proto --python_out=.
 cd /cnvrg
